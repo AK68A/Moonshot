6c1eb7585cc900da27508c959274351c3ff20867
AK68A <a@ak68a.co>
2021-02-09 22:56:51 -0500

    Load decodable data

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Vayu Moreira on 2/9/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    var id: String
+    var name: String
+    var description: String
+}
